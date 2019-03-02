540f2e0cb81cc1bfda396babb3ae4670a66d5dc3
chagri ali <chagriali@gmail.com>
2019-03-02 15:47:14 +0100

    Sixth module commit

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,16 +3,35 @@ import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component  {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            incorrectAnswer : false
+        }
+    }
+
     handleClick(button_text){
         if(this.props.quiz_question.answer === button_text)
         {
+            this.setState({
+                incorrectAnswer : false
+            })
             this.props.showNextQuestionHandler();
+        }else{
+            this.setState({
+                incorrectAnswer : true 
+            });
         }
     }
 
     render(){
         return (
             <main>
+                {
+                    this.state.incorrectAnswer === true ? 
+                    <p className='error'> Sorry, that's not right </p> :
+                    null
+                }
                 <section>
                 <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
